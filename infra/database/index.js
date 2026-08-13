5eeb57495cd5e90fdcdb857b101dae59fdc29305
Marcelo Cruz <mfugissecruz@gmail.com>
2026-08-13 13:39:45 -0300

    fix: handle client as undefined in database.j

--- infra/database/index.js
+++ infra/database/index.js
@@ -6,9 +6,11 @@ async function query(queryObject) {
     client = await getNewClient();
     return await client.query(queryObject);
   } catch (error) {
+    console.log("\nErro dentro do catch do database.js");
     console.error(error);
+    throw error;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 
